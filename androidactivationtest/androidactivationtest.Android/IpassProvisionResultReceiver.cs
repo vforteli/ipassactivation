04010915e331f8727f87fd798aa73265fd605912
Unknown <verner@fortelius.net>
2017-08-20 00:02:11 +0300

    Initial commit

---
+++ androidactivationtest/androidactivationtest.Android/IpassProvisionResultReceiver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace androidactivationtest.Droid
+{
+    [BroadcastReceiver(Enabled = true, Exported = true)]
+    [IntentFilter(new[] { "client_x.android.prov.result" })]
+    public class IpassProvisionResultReceiver : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            var status = intent.GetIntExtra("EXTRA_RESULT", -1);
+            Toast.MakeText(context, $"Power Connected {status}", ToastLength.Long).Show();
+        }
+    };
+}
---
+++ androidactivationtest/androidactivationtest.Android/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace androidactivationtest.Droid
+{
+    [Activity(Label = "androidactivationtest.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
+
+            button.Click += delegate
+            {
+                button.Text = $"{count++} clicks";
+                ActivateIpass("10691", "8854");
+            };
+        }
+
+
+        private void ActivateIpass(String profileId, String pin)
+        {
+            var provisionIntent = new Intent("android.intent.action.VIEW");            
+            provisionIntent.SetData(Android.Net.Uri.Parse($"clientx://provision?pid={profileId}&pin={pin}"));
+            Application.Context.StartService(provisionIntent);
+        }
+    }
+}
+
+
